ee483f7e4298f71f53c9b1090d493acc7a5a2b24
Baraka Mulumia <barakamulumia@gmail.com>
2021-07-14 17:37:33 +0300

    Create CustomerDashBoardActivity.kt

---
+++ app/src/main/java/com/baraka/foodmate/CustomerDashBoardActivity.kt
@@ -0,0 +1,11 @@
+package com.baraka.foodmate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomerDashBoardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_dash_board)
+    }
+}
