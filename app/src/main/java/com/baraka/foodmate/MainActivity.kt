3e235b689b4eab32af0c3c0685e2ec846027d851
Baraka Mulumia <barakamulumia@gmail.com>
2021-07-15 16:00:57 +0300

    Update MainActivity.kt
    Code FORMATING

--- app/src/main/java/com/baraka/foodmate/MainActivity.kt
+++ app/src/main/java/com/baraka/foodmate/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.baraka.foodmate
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
@@ -14,28 +14,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         auth = FirebaseAuth.getInstance()
-        val currentUser = auth.currentUser;
-        if(currentUser != null){
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
             startActivity(Intent(this, CustomerDashBoardActivity::class.java))
             finish()
         }
 
-
-        Toast.makeText(this, "Welcome to our Resturant",
+        Toast.makeText(
+            this, "Welcome to our Resturant",
             Toast.LENGTH_LONG
         ).show()
 
-        val loginButton: Button =findViewById(R.id.button_login)
+        val loginButton: Button = findViewById(R.id.button_login)
         val signUpButton: Button = findViewById(R.id.button_signup)
 
-        loginButton.setOnClickListener{
+        loginButton.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
 
-        signUpButton.setOnClickListener{
+        signUpButton.setOnClickListener {
             val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
