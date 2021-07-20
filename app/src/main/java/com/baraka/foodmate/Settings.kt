ab2d504aa47420787f553e57536ea6d4a0be05de
Baraka Mulumia <barakamulumia@gmail.com>
2021-07-20 10:46:41 +0300

    Update Settings.kt
    Testing out find view by id using a Toast

--- app/src/main/java/com/baraka/foodmate/Settings.kt
+++ app/src/main/java/com/baraka/foodmate/Settings.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 
 
 class Settings : Fragment() {
@@ -17,5 +19,14 @@ class Settings : Fragment() {
         return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val editButton: Button = view.findViewById(R.id.edit_profile_button)
+
+        editButton.setOnClickListener{
+            Toast.makeText(context, "Button clicked", Toast.LENGTH_LONG).show()
+        }
+
+    }
 }
