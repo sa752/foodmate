e63a3a9ae5e93b74f1e3ba18ff775fdd7fdfef26
Baraka Mulumia <barakamulumia@gmail.com>
2021-07-21 18:18:56 +0300

    Update Settings.kt
    Added Missing import for Toast Module

--- app/src/main/java/com/baraka/foodmate/Settings.kt
+++ app/src/main/java/com/baraka/foodmate/Settings.kt
@@ -9,6 +9,7 @@ import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.storage.FirebaseStorage
