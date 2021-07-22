3bdfcaba0920f14830f769e5b1182345de784a68
Baraka Mulumia <barakamulumia@gmail.com>
2021-07-22 09:11:32 +0300

    Update CustomerDashBoardActivity.kt
    Added setUserFullNameAndEmail  to get user info from Firebase Database

--- app/src/main/java/com/baraka/foodmate/CustomerDashBoardActivity.kt
+++ app/src/main/java/com/baraka/foodmate/CustomerDashBoardActivity.kt
@@ -2,9 +2,7 @@ package com.baraka.foodmate
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.Gravity
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
@@ -12,16 +10,22 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.nav_header.*
 
 class CustomerDashBoardActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     lateinit var toggle: ActionBarDrawerToggle
     lateinit var auth: FirebaseAuth
+    var databaseReference: DatabaseReference? = null
+    var database: FirebaseDatabase? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customer_dash_board)
 
         auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+        databaseReference = database?.reference?.child("profile")
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
@@ -40,6 +44,8 @@ class CustomerDashBoardActivity : AppCompatActivity(), NavigationView.OnNavigati
         //automatic fragment
         val fragment = supportFragmentManager.beginTransaction()
         fragment.replace(R.id.fragment_container, Home()).commit()
+
+        setUserFullNameAndEmail()
 
     }
 
@@ -88,6 +94,23 @@ class CustomerDashBoardActivity : AppCompatActivity(), NavigationView.OnNavigati
     private fun changeFragment(fragName :Fragment){
         val fragment = supportFragmentManager.beginTransaction()
         fragment.replace(R.id.fragment_container, fragName).commit()
+    }
+
+    private  fun setUserFullNameAndEmail(){
+
+        val user = auth.currentUser;
+        var userReference = databaseReference?.child(user?.uid!!)
+
+
+        userReference?.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                nav_bar_user_name.text = snapshot.child("firstname").value.toString() + " "+ snapshot.child("lastname").value.toString()
+           nav_bar_user_email.setText(user?.email)
+            }
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 
 }
