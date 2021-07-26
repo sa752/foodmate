7c0b7ec2b7bee86729ec156c57c424c14a04b10a
Baraka Mulumia <barakamulumia@gmail.com>
2021-07-26 08:06:26 +0300

    Update Home.kt
    Added Recycler View for Meal List Items

--- app/src/main/java/com/baraka/foodmate/Home.kt
+++ app/src/main/java/com/baraka/foodmate/Home.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class Home : Fragment() {
+    private lateinit var mealAdapter: MealRecyclerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,5 +20,25 @@ class Home : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecyclerView()
+        addDataSet()
+    }
+
+    private fun initRecyclerView() {
+        home_page_recycler_view.apply {
+            layoutManager = LinearLayoutManager(context)
+           val topSpacingItemDecoration = TopSpacingItemDecoration(30)
+            addItemDecoration(topSpacingItemDecoration)
+            mealAdapter = MealRecyclerAdapter()
+            adapter = mealAdapter
+        }
+    }
+
+    private  fun addDataSet(){
+        val data = DataSource.createDataSet()
+        mealAdapter.submitList(data)
+    }
 
 }
