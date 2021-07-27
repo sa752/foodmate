8ee202a7f72e3dea8619dd4d06ad9dc99bdcf821
Baraka Mulumia <barakamulumia@gmail.com>
2021-07-27 12:53:17 +0300

    Revert "Refactor"
    This reverts commit 5d46b7c16b0b9385f03659f229eb19a26c75dc20.

--- app/src/main/java/com/baraka/foodmate/Meals.kt
+++ app/src/main/java/com/baraka/foodmate/Meals.kt
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 
 
-class MyOrders : Fragment() {
+class Meals : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_myorders, container, false)
+        return inflater.inflate(R.layout.fragment_meals, container, false)
     }
 
 }
